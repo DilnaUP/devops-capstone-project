f45129cc82bb2dced79e0dc046c892a55debfa38
DilnaUP <168866299+DilnaUP@users.noreply.github.com>
2024-05-08 20:52:36 -0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -88,7 +88,7 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND,
-        f"Account with id [{account_id}] could not be found.")
+              f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -105,7 +105,8 @@ def update_accounts(account_id):
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(status.HTTP_404_NOT_FOUND,
+              f"Account with id [{account_id}] could not be found.")
         account.deserialize(request.get_json())
         account.update()
     return account.serialize(), status.HTTP_200_OK
@@ -130,6 +131,8 @@ def delete_accounts(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
+
+
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
